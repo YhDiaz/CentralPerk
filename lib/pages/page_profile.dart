badb1c8525ca1e2aef57f76e9ffbbf42eab6eb2a
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-09-25 01:14:11 -0300

    Add footer buttons in profile page - Add footer buttons to navigate in profile page. - Add home button with outlined icon. - Add search button with outlined icon. - Add shop button with outlined icon. - Add profile button. - Create a static const variable to store space between buttons value. - Note 1: all the buttons have their functionality implemented. - Note 2: footer buttons in this page have the improved navigation logic implemented.

--- lib/pages/page_profile.dart
+++ lib/pages/page_profile.dart
@@ -1,3 +1,5 @@
+import 'package:central_perk/pages/page_search.dart';
+import 'package:central_perk/pages/page_shop.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget
@@ -11,6 +13,42 @@ class ProfilePage extends StatefulWidget
 
 class _ProfilePageState extends State<ProfilePage>
 {
+  static const double spaceBetweenButtons = 15; //Space between footer buttons
+
+  void _goToHomePage()
+  {
+    setState(()
+    {
+      Navigator.pop(context);
+    });
+  }
+
+  void _goToSearchPage()
+  {
+    setState(()
+    {
+      Navigator.pop(context);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage(title: 'Central Perk - Search page')));
+    });
+  }
+
+  void _goToShopPage()
+  {
+    setState(()
+    {
+      Navigator.pop(context);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const ShopPage(title: 'Central Perk - Shop page')));      
+    });
+  }
+
+  void _goToProfilePage()
+  {
+    setState(()
+    {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context)
   {
@@ -20,7 +58,24 @@ class _ProfilePageState extends State<ProfilePage>
       (
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      )
+      ),
+      persistentFooterButtons: 
+      [
+        Row
+        (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:
+          [
+            ElevatedButton(onPressed: _goToHomePage, child: const Icon(Icons.home_outlined)), //Home button
+            const SizedBox(width: spaceBetweenButtons),
+            ElevatedButton(onPressed: _goToSearchPage, child: const Icon(Icons.search_outlined)), //Search button
+            const SizedBox(width: spaceBetweenButtons),
+            ElevatedButton(onPressed: _goToShopPage, child: const Icon(Icons.shop_outlined)), //Shop button
+            const SizedBox(width: spaceBetweenButtons),
+            ElevatedButton(onPressed: _goToProfilePage, child: const Icon(Icons.person)), //Profile button
+          ],
+        )
+      ],
     );
   }
 }
