0de43d6c428d5f92b122ad915774efdacd3b1227
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-09-25 09:48:19 -0300

    Implement body in home page - Implement CarouselView to display cards. - Create recipe Card with basic structure and format. - Note: the recipe Card display a recipe image, info about recipe name and publication date, and creator's user profile

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -14,7 +14,6 @@ class HomePage extends StatefulWidget
 
 class _HomePageState extends State<HomePage>
 {
-  final int _counter = 0;
   static const double spaceBetweenButtons = 15; //Space between footer buttons
 
   void _goToHomePage()
@@ -61,22 +60,343 @@ class _HomePageState extends State<HomePage>
       ),
       body: Center
       (
-        child: Column
+        child: ConstrainedBox
         (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>
-          [
-            const Text
-            (
-              'You have pushed the button this many times:',
-            ),
-            Text
-            (
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          constraints: const BoxConstraints(minWidth: 350, maxWidth: 400),
+          child:CarouselView
+          (
+            scrollDirection: Axis.vertical,
+            itemExtent: 100,
+            shrinkExtent: 50,
+            children: List<Widget>.generate(10, (int index) {
+              // return Center(child: Text('Item $index'));
+              return Card
+              (
+                margin: const EdgeInsets.all(10),
+                child: Row
+                (
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: 
+                  [
+                    const Padding(padding: EdgeInsets.all(5)),
+                    const SizedBox //Recipe image
+                    (
+                      height: 75,
+                      width: 75,
+                      child: Icon(Icons.coffee_outlined),
+                    ),
+                    const SizedBox(width: 15,), //Space between recipe image and info
+                    const Column //Info: recipe name and publish date
+                    (
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children:
+                      [
+                        Text('Mokaccino'),
+                        Text('September 25, 2024')
+                      ],
+                    ),
+                    const SizedBox(width: 75,), //Space between info and user
+                    SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+                  ],
+                ),
+              );
+            }),
+          ),
         ),
+        // child: ListBody
+        // (
+        //   mainAxis: Axis.vertical,
+        //   children:
+        //   [
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+
+        //     Card
+        //     (
+        //       margin: const EdgeInsets.all(10),
+        //       child: Row
+        //       (
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: 
+        //         [
+        //           const Padding(padding: EdgeInsets.all(10)),
+        //           const SizedBox //Recipe image
+        //           (
+        //             height: 75,
+        //             width: 75,
+        //             child: Icon(Icons.coffee_outlined),
+        //           ),
+        //           const SizedBox(width: 10,),
+        //           const Column //Info: recipe name and publish date
+        //           (
+        //             mainAxisSize: MainAxisSize.min,
+        //             mainAxisAlignment: MainAxisAlignment.start,
+        //             children:
+        //             [
+        //               Text('Mokaccino'),
+        //               Text('September 25, 2024')
+        //             ],
+        //           ),
+        //           const SizedBox(width: 25,),
+        //           SizedBox.square(dimension: 50, child: IconButton(onPressed: _goToProfilePage, icon: const Icon(Icons.person)))
+        //         ],
+        //       ),
+        //     ),
+        //   ],
+        // )
+        // child: ListBody
+        // (
+        //   mainAxis: Axis.vertical,
+        //   children:
+        //   [
+        //     Card
+        //     (
+        //       child: Row(
+        //         children: [
+        //           Text('Hola'),
+        //         ],
+        //       ),
+        //     ),
+        //   ],
+        // )
+        // child: Column
+        // (
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>
+        //   [
+        //     const Text
+        //     (
+        //       'You have pushed the button this many times:',
+        //     ),
+        //     Text
+        //     (
+        //       '$_counter',
+        //       style: Theme.of(context).textTheme.headlineMedium,
+        //     ),
+        //   ],
+        // ),
       ),
       persistentFooterButtons: 
       [
