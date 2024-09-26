9b0f02cd5ef7488135e70cae559deda0f3795170
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-09-26 16:42:23 -0300

    Update to standard nomenclature

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:central_perk/pages/page_home.dart';
 
-void main()
-{
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-    (
+  Widget build(BuildContext context) {
+    return MaterialApp (
       title: 'Central Perk',
-      theme: ThemeData
-      (
+      theme: ThemeData (
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -27,8 +22,7 @@ class MyApp extends StatelessWidget
   }
 }
 
-class SplashPage extends StatefulWidget
-{
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
@@ -36,19 +30,14 @@ class SplashPage extends StatefulWidget
   _SplashPageState createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage>
-{
+class _SplashPageState extends State<SplashPage> {
   @override
-  Widget build(BuildContext context)
-  {
-    return EasySplashScreen
-    (
+  Widget build(BuildContext context) {
+    return EasySplashScreen (
       logo: Image.asset('assets/icons/icon_central_perk_logo.png'),
-      title: const Text
-      (
+      title: const Text (
         "Central Perk",
-        style: TextStyle
-        (
+        style: TextStyle (
           fontSize: 25,
           fontWeight: FontWeight.bold,
         ),
