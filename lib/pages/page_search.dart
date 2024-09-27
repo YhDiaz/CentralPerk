535f9e7fac362e1de87d3d81fc5e49543bfb7e8a
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-09-27 18:36:44 -0300

    Adjust search page aesthetics

--- lib/pages/page_search.dart
+++ lib/pages/page_search.dart
@@ -68,8 +68,7 @@ List<Recipe> recipes = [
 ];
 
 
-class SearchPage extends StatefulWidget
-{
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key, required this.title});
   final String title;
 
@@ -84,58 +83,48 @@ class _SearchPageState extends State<SearchPage>
   static const Color appBarColor = Color(0xFF66280a);
   static const Color appBarTextColor = Color(0xFFF2E0D3);
 
-  final ButtonStyle defaultButton = ElevatedButton.styleFrom //Footer buttons non-linked to current page
-  (
+  // Footer buttons non-linked to current page.
+  final ButtonStyle defaultButton = ElevatedButton.styleFrom(
     iconColor: appBarColor,
     backgroundColor: footerBarColor
   );
-  final ButtonStyle selectedButton = ElevatedButton.styleFrom //Footer button linked to current page
-  (
+
+  // Footer button linked to current page.
+  final ButtonStyle selectedButton = ElevatedButton.styleFrom(
     iconColor: appBarColor,
     backgroundColor: const Color(0xFFD1B6A3)
   );
 
-  void _goToHomePage()
-  {
-    setState(()
-    {
+  void _goToHomePage() {
+    setState(() {
       Navigator.pop(context); 
     });
   }
 
-  void _goToSearchPage()
-  {
-    setState(()
-    {
+  void _goToSearchPage() {
+    setState(() {
 
     });
   }
 
-  void _goToShopPage()
-  {
-    setState(()
-    {
+  void _goToShopPage() {
+    setState(() {
       Navigator.pop(context);
       Navigator.push(context, MaterialPageRoute(builder: (context) => const ShopPage(title: 'Central Perk - Shop page')));
     });
   }
 
-  void _goToProfilePage()
-  {
-    setState(()
-    {
+  void _goToProfilePage() {
+    setState(() {
       Navigator.pop(context);
       Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfilePage(title: 'Central Perk - Profile page'))); 
     });
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         backgroundColor: appBarColor,
         title: Text(widget.title, style: const TextStyle(color: appBarTextColor)),
       ),
@@ -179,13 +168,13 @@ class _SearchPageState extends State<SearchPage>
             ),
             const SizedBox(height: 20,),
             ConstrainedBox(
-              constraints: const BoxConstraints(
+              constraints: const BoxConstraints( // Scroll box size.
                 minHeight: 350,
                 maxHeight: 525,
               ),
               child: CarouselView(
                 scrollDirection: Axis.vertical,
-                itemExtent: 200,
+                itemExtent: 125, // Item size (in this case, height).
                 shrinkExtent: 0.0,
                 children: List<Widget>.generate(5, (int index) {
                   return generateCard(index);
@@ -195,16 +184,12 @@ class _SearchPageState extends State<SearchPage>
           ],
         ),
       ),
-      persistentFooterButtons: 
-      [
-        ColoredBox
-        (
+      persistentFooterButtons: [
+        ColoredBox(
           color: footerBarColor,
-          child: Row
-          (
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:
-            [
+            children: [
               FilledButton(onPressed: _goToHomePage, style: defaultButton, child: const Icon(Icons.home_outlined)), //Home button
               const SizedBox(width: spaceBetweenButtons),
               FilledButton(onPressed: _goToSearchPage, style: selectedButton, child: const Icon(Icons.search)), //Search button
