ea7e7fd4f6e4866fe6b2d32eba9b2a756ce291c1
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-10-01 01:35:55 -0300

    Implement functions and container - Create static globalId to assign id based on it. - Add string variable for steps to execute recipe. - Remove recomendations list. - Change data type of comments list. - Implement logic to assign id in constructor. - Implement function setFav to set favorite value at the same time that update owner's favorite recipes. - Implement functions to print messages for ingredients, comments and artifacts in recipe page.

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,21 +1,25 @@
 import 'package:central_perk/models/coffe_bean_type.dart';
+import 'package:central_perk/models/comment.dart';
+import 'package:central_perk/models/product.dart';
 import 'package:central_perk/models/recipe_visibility.dart';
 import 'package:central_perk/models/user.dart';
 
 class Recipe {
+  static int globalId = 0;
+  String id = '';
+
   final String name;
   final DateTime date;
-  final int id = 0;
   final User owner;
   final List<String> ingredients = List.empty(growable: true);
   final double time;
   final String description;
+  final String steps;
   final String image;
   final RecipeVisibility visibility;
   final List<String> tags = List.empty(growable: true);
-  final List<String> recomendations = List.empty(growable: true);
-  final List<String> comments = List.empty(growable: true);
-  // final List<Product> artifacts = List.empty(growable: true);
+  final List<Comment> comments = List.empty(growable: true);
+  final List<Product> artifacts = List.empty(growable: true);
   final CoffeBeanType coffeBean;
   
   double rating = 1.0;
@@ -28,21 +32,63 @@ class Recipe {
     required this.owner,
     required this.time,
     required this.description,
+    required this.steps,
     required this.image,
     required this.visibility,
     required this.coffeBean,
     this.rating = 1.0,
     this.likes = 0,
     this.favorite = false
-  });
-
-  @override
-  String toString() {
-    // TODO: implement toString
-    return super.toString();
+  })
+  {
+    globalId++;
+    id = name + globalId.toString();
   }
 
-  // void markAsFav() {
-  //   favorite = true;
-  // }
+  void setFav(bool value) {
+    favorite = value;
+
+    // Make changes in owner favorite recipes list.
+    if (favorite) {
+      owner.markRecipeAsFav(this);
+    } else {
+      owner.removeFromFav(this);
+    }
+  }
+
+  String printIngredients() {
+    String ingredientsMessage = '';
+
+    for (int i = 0; i < ingredients.length; i++) {
+      ingredientsMessage += '${i + 1}: ${ingredients[i]}\n';
+    }
+
+    return (ingredientsMessage == '') ? 
+            'There are no ingredients for this recipe.' :
+            ingredientsMessage;
+  }
+
+  String printArtifacts() {
+    String artifactsMessage = '';
+
+    for (int i = 0; i < artifacts.length; i++) {
+      artifactsMessage += '${i + 1}: ${artifacts[i].name}\n';
+    }
+
+    return (artifactsMessage == '') ? 
+            'You don\'t need any artifacts for this recipe.' :
+            artifactsMessage;
+  }
+
+  String printComments() {
+    String commentsMessage = '';
+
+    for (int i = 0; i < comments.length; i++) {
+      commentsMessage += '${comments[i].owner.name} - ${comments[i].date.day}/${comments[i].date.month}/${comments[i].date.year}\n${comments[i].comment}\n';
+    }
+
+    return (commentsMessage == '') ? 
+            'There aren\'t any comments for this recipe.' :
+            commentsMessage;
+  }
 }
