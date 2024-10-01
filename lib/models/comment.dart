ca759680c9e0c3018aea9c6bb92bdc6f076e6541
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-10-01 01:41:09 -0300

    Create comment class

---
+++ lib/models/comment.dart
@@ -0,0 +1,13 @@
+import 'package:central_perk/models/user.dart';
+
+class Comment {
+  final User owner;
+  final DateTime date;
+  final String comment;
+
+  Comment({
+    required this.owner,
+    required this.date,
+    required this.comment
+  });
+}
