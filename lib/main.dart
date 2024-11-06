4e3ea419115cd6443cf5dec98b123b22397567ec
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-11-06 00:46:32 -0300

    Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:central_perk/models/recipe_manager.dart';
 import 'package:central_perk/models/recipe_visibility.dart';
 import 'package:central_perk/models/user_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_splash_screen/easy_splash_screen.dart';
+// import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:central_perk/pages/page_home.dart';
 
 void main() {
@@ -226,37 +226,7 @@ class MyApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary
         )
       ),
-      // home: const SplashPage(),
       home: const HomePage(title: 'Central perk - Home page')
     );
   }
 }
-
-// class SplashPage extends StatefulWidget {
-//   const SplashPage({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _SplashPageState createState() => _SplashPageState();
-// }
-
-// class _SplashPageState extends State<SplashPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return EasySplashScreen (
-//       logo: Image.asset('assets/icons/icon_central_perk_logo.png'),
-//       title: const Text (
-//         "Central Perk",
-//         style: TextStyle (
-//           fontSize: 25,
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//       backgroundColor: const Color(0xFF724A22),
-//       showLoader: true,
-//       loadingText: const Text("Opening coffeehouse..."),
-//       navigator: const HomePage(title: 'Central Perk - Home page'),
-//       durationInSeconds: 5,
-//     );
-//   }
-// }
