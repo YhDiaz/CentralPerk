5c8628205c6fb868e14e4c28e9cb58c83cd217df
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-11-07 11:56:46 -0300

    Organize code - Create method to get list of recipes to display depending on the load state of recipes. - Clean code.

--- lib/pages/page_my_barista.dart
+++ lib/pages/page_my_barista.dart
@@ -18,7 +18,6 @@ class _MyBaristaPageState extends State<MyBaristaPage> {
   @override
   void initState() {
     super.initState();
-    print("en el init state del barista");
     _loadRecipes();
     _recipes = _buildMyBaristaRecipesList();
   }
@@ -43,15 +42,32 @@ class _MyBaristaPageState extends State<MyBaristaPage> {
 
   @override
   Widget build(BuildContext context) {
-    print("players es: ${_recipes.toList()}");
-
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.displayMedium!,
       textAlign: TextAlign.center,
       child: FutureBuilder<List<Recipe>>(
         future: _recipesFuture, // a previously-obtained Future<String> or null
         builder: (BuildContext context, AsyncSnapshot<List<Recipe>> snapshot) {
-          Widget expanded = Column(
+          Widget recipesList = _getRecipesListToDisplay(true); // Base Widget.
+
+          if (snapshot.hasData) recipesList = _getRecipesListToDisplay(false); // List of recipes.
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title, style: const TextStyle(color: appBarTextColor)),
+            ),
+            body: recipesList
+          );
+        },
+      ),
+    );
+  }
+
+  // Get list of My barista recipes to display.
+  // defaultInfo = true: indicates that there aren't any recipes loaded yet.
+  // defaultInfo = false: indicate that My barista recipes were loaded.
+  Widget _getRecipesListToDisplay(bool defaultInfo) {
+    return Column(
             children: [
               Expanded( // To avoid problems due to include a ListView inside a Column.
                 child: SizedBox(
@@ -59,77 +75,15 @@ class _MyBaristaPageState extends State<MyBaristaPage> {
                   child: ListView.builder(
                     itemCount: _recipes.length,
                     itemBuilder: (context, index) {
-                      // return _recipes[index].getCard(context);
-                      return Text('no recipe $index');
+                      return defaultInfo ?
+                        Text('No se encontró información de la receta $index') // Recipes aren't loaded yet.
+                      :
+                        _recipes[index].getCard(context); // Display recipe card.
                     },
                   )
                 ),
               )
             ],
           );
-          if (snapshot.hasData) {
-            expanded = Column(
-              children: [
-                Expanded( // To avoid problems due to include a ListView inside a Column.
-                  child: SizedBox(
-                    width: 350.0, // Width of list items.
-                    child: ListView.builder(
-                      itemCount: _recipes.length,
-                      itemBuilder: (context, index) {
-                        return _recipes[index].getCard(context);
-                        // return Text('recipe has data $index');
-                      },
-                    )
-                  ),
-                )
-              ],
-            );
-          }
-          else if (snapshot.hasError) {
-            expanded = Column(
-              children: [
-                Expanded( // To avoid problems due to include a ListView inside a Column.
-                  child: SizedBox(
-                    width: 350.0, // Width of list items.
-                    child: ListView.builder(
-                      itemCount: _recipes.length,
-                      itemBuilder: (context, index) {
-                        // return _recipes[index].getCard(context);
-                        return Text('recipe has error $index');
-                      },
-                    )
-                  ),
-                )
-              ],
-            );
-          } else {
-            expanded = Column(
-              children: [
-                Expanded( // To avoid problems due to include a ListView inside a Column.
-                  child: SizedBox(
-                    width: 350.0, // Width of list items.
-                    child: ListView.builder(
-                      itemCount: _recipes.length,
-                      itemBuilder: (context, index) {
-                        // return _recipes[index].getCard(context);
-                        return Text('');
-                      },
-                    )
-                  ),
-                )
-              ],
-            );
-          }
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title, style: const TextStyle(color: appBarTextColor)),
-            ),
-            body: expanded
-            
-          );
-        },
-      ),
-    );
   }
 }
