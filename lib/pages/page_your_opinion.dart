78171cc9c2fab902cc65fc95cb212eb0f3a9ef43
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-11-07 09:29:48 -0300

    Try to implement email sender package

--- lib/pages/page_your_opinion.dart
+++ lib/pages/page_your_opinion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class YourOpinionPage extends StatefulWidget {
   const YourOpinionPage({super.key});
@@ -8,11 +9,32 @@ class YourOpinionPage extends StatefulWidget {
 }
 
 class _YourOpinionPageState extends State<YourOpinionPage> {
+  Future<void> _sendEmail() async {
+    final Email email = Email(
+            body: 'Test successful, direct to spam.',
+            subject: 'Testing email sender package',
+            recipients: ['yhdiazofficial@gmail.com'],
+            // cc: ['cc@example.com'],
+            // bcc: ['bcc@example.com'],
+            // attachmentPaths: ['/path/to/attachment.zip'],
+            isHTML: false,
+          );
+
+          await FlutterEmailSender.send(email);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tu opinión'),
+      ),
+      body: Center(
+        child: Text('Preguntas')
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendEmail,
+        child: Icon(Icons.email)
       ),
     );
   }
